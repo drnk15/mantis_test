b70e690ff097a8dfd053364edc8ecd0a6529adb9
natalia <konv.42@gmail.com>
2022-01-06 19:49:15 +0300

    test_login implemented

---
+++ fixture/application.py
@@ -0,0 +1,32 @@
+from selenium import webdriver
+from fixture.session import SessionHelper
+
+
+class Application:
+    def __init__(self, browser, base_url):
+        if browser == "firefox":
+            self.wd = webdriver.Firefox()
+        elif browser == "chrome":
+            self.wd = webdriver.Chrome()
+        elif browser == "edge":
+            self.wd = webdriver.Edge()
+        else:
+            raise ValueError(f"Unrecognized browser {browser}!")
+        self.wd.implicitly_wait(10)
+        self.session = SessionHelper(self)
+        self.base_url = base_url
+
+    def is_valid(self):
+        try:
+            self.wd.current_url
+            return True
+        except:
+            return False
+
+    def destroy(self):
+        self.wd.quit()
+
+    def open_home_page(self):
+        wd = self.wd
+        if not wd.current_url.endswith("/mantisbt-2.25.2/"):
+            wd.get(self.base_url)
